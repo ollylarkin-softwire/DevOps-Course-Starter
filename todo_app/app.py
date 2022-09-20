d1bb73b4025bd4fee7e6ca02d3479658cc0178c9
Olly Larkin <olly.larkin@softwire.com>
2022-09-20 15:51:07 +0100

    EX-3: Use view model

--- todo_app/app.py
+++ todo_app/app.py
@@ -7,18 +7,38 @@ from todo_app.flask_config import Config
 app = Flask(__name__)
 app.config.from_object(Config())
 
-TITLE_FIELD_NAME = 'title' 
+TITLE_FIELD_NAME = 'title'
+
+class ViewModel:
+    def __init__(self, title_field_name, items):
+        self._title_field_name = title_field_name
+        self._not_started_todos = filter(lambda t: t.status == NOT_STARTED_STATUS, items)
+        self._doing_todos = filter(lambda t: t.status == DOING_STATUS, items)
+        self._done_todos = filter(lambda t: t.status == DONE_STATUS, items)
+
+    @property
+    def title_field_name(self):
+        return self._title_field_name
+
+    @property
+    def not_started_todos(self):
+        return self._not_started_todos
+
+    @property
+    def doing_todos(self):
+        return self._doing_todos
+
+    @property
+    def done_todos(self):
+        return self._done_todos
 
 @app.route('/')
 def index():
     todos = get_items()
-    return render_template(
-        'index.html',
+    return render_template('index.html', view_model=ViewModel(
         title_field_name=TITLE_FIELD_NAME,
-        not_started_todos=filter(lambda t: t.status == NOT_STARTED_STATUS, todos),
-        doing_todos=filter(lambda t: t.status == DOING_STATUS, todos),
-        done_todos=filter(lambda t: t.status == DONE_STATUS, todos),
-    )
+        items=todos,
+    ))
 
 @app.route('/new-todo', methods=['POST'])
 def new_todo():
