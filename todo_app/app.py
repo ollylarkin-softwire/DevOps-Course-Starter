885b3638f5762cc028efd3c1572e5e597578f9b3
Olly Larkin <olly.larkin@softwire.com>
2022-09-21 16:52:20 +0100

    EX-3: Add integration test and other small changes

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,41 +1,45 @@
 from flask import Flask, render_template, request, redirect, url_for
-from todo_app.data.trello_items import get_items, add_item, start_item, complete_item, reset_item
+from todo_app.data.trello_items import TrelloItems
 from todo_app.view_model import ViewModel
 
 from todo_app.flask_config import Config
 
-app = Flask(__name__)
-app.config.from_object(Config())
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config())
+    trello_items = TrelloItems()
 
-TITLE_FIELD_NAME = 'title'
+    TITLE_FIELD_NAME = 'title'
 
-@app.route('/')
-def index():
-    todos = get_items()
-    should_show_all_done_items = request.args.get('should_show_all_done_items', default=False, type=bool)
-    return render_template('index.html', view_model=ViewModel(
-        title_field_name=TITLE_FIELD_NAME,
-        items=todos,
-        should_show_all_done_items=should_show_all_done_items,
-    ))
+    @app.route('/')
+    def index():
+        todos = trello_items.get_items()
+        should_show_all_done_items = request.args.get('should_show_all_done_items', default=False, type=bool)
+        return render_template('index.html', view_model=ViewModel(
+            title_field_name=TITLE_FIELD_NAME,
+            items=todos,
+            should_show_all_done_items=should_show_all_done_items,
+        ))
 
-@app.route('/new-todo', methods=['POST'])
-def new_todo():
-    title = request.form.get(TITLE_FIELD_NAME)
-    add_item(title)
-    return redirect(url_for('index'))
+    @app.route('/new-todo', methods=['POST'])
+    def new_todo():
+        title = request.form.get(TITLE_FIELD_NAME)
+        trello_items.add_item(title)
+        return redirect(url_for('index'))
 
-@app.route('/start-todo/<id>', methods=['GET'])
-def start_todo(id):
-    start_item(id)
-    return redirect(url_for('index'))
+    @app.route('/start-todo/<id>', methods=['GET'])
+    def start_todo(id):
+        trello_items.start_item(id)
+        return redirect(url_for('index'))
 
-@app.route('/complete-todo/<id>', methods=['GET'])
-def complete_todo(id):
-    complete_item(id)
-    return redirect(url_for('index'))
+    @app.route('/complete-todo/<id>', methods=['GET'])
+    def complete_todo(id):
+        trello_items.complete_item(id)
+        return redirect(url_for('index'))
 
-@app.route('/reset-todo/<id>', methods=['GET'])
-def reset_todo(id):
-    reset_item(id)
-    return redirect(url_for('index'))
+    @app.route('/reset-todo/<id>', methods=['GET'])
+    def reset_todo(id):
+        trello_items.reset_item(id)
+        return redirect(url_for('index'))
+
+    return app
--- todo_app/data/trello_items.py
+++ todo_app/data/trello_items.py
@@ -2,71 +2,73 @@ from todo_app.data.trello_api_utils import Trello
 from todo_app.data.item import Item
 import requests
 
-trello = Trello()
+class TrelloItems:
+    def __init__(self):
+        self.trello = Trello()
 
-def get_items():
-    """
-    Fetches all open cards from Trello board
+    def get_items(self):
+        """
+        Fetches all open cards from Trello board
 
-    Returns:
-        list: The list of open cards
-    """
-    r = trello.request(f'/boards/{trello.board_id}/cards/open')
-    r.raise_for_status()
-    cards = r.json()
-    return [Item.from_trello(trello, card) for card in cards]
-
-
-def get_item(id):
-    """
-    Fetches the saved card with the specified ID
-
-    Args:
-        id: The ID of the card
-
-    Returns:
-        item: The saved item, or None if no items match the specified ID.
-    """
-    r = trello.request(f'/cards/{id}')
-    if r.status_code == requests.codes.not_found:
-        return None
-    r.raise_for_status()
-    card = r.json()
-    return Item.from_trello(trello, card)
+        Returns:
+            list: The list of open cards
+        """
+        r = self.trello.request(f'/boards/{self.trello.board_id}/cards/open')
+        r.raise_for_status()
+        cards = r.json()
+        return [Item.from_trello(self.trello, card) for card in cards]
 
 
-def add_item(title):
-    r = trello.request('/cards', 'POST', {
-        'name': title,
-        'idList': trello.list_ids.not_started,
-    })
-    r.raise_for_status()
-    added_card = r.json()
-    return Item.from_trello(trello, added_card)
+    def get_item(self, id):
+        """
+        Fetches the saved card with the specified ID
+
+        Args:
+            id: The ID of the card
+
+        Returns:
+            item: The saved item, or None if no items match the specified ID.
+        """
+        r = self.trello.request(f'/cards/{id}')
+        if r.status_code == requests.codes.not_found:
+            return None
+        r.raise_for_status()
+        card = r.json()
+        return Item.from_trello(self.trello, card)
 
 
-def start_item(id):
-    r = trello.request(f'/cards/{id}', 'PUT', {
-        'idList': trello.list_ids.doing,
-    })
-    r.raise_for_status()
-    updated_card = r.json()
-    return Item.from_trello(trello, updated_card)
+    def add_item(self, title):
+        r = self.trello.request('/cards', 'POST', {
+            'name': title,
+            'idList': self.trello.list_ids.not_started,
+        })
+        r.raise_for_status()
+        added_card = r.json()
+        return Item.from_trello(self.trello, added_card)
 
 
-def complete_item(id):
-    r = trello.request(f'/cards/{id}', 'PUT', {
-        'idList': trello.list_ids.done,
-    })
-    r.raise_for_status()
-    updated_card = r.json()
-    return Item.from_trello(trello, updated_card)
+    def start_item(self, id):
+        r = self.trello.request(f'/cards/{id}', 'PUT', {
+            'idList': self.trello.list_ids.doing,
+        })
+        r.raise_for_status()
+        updated_card = r.json()
+        return Item.from_trello(self.trello, updated_card)
 
 
-def reset_item(id):
-    r = trello.request(f'/cards/{id}', 'PUT', {
-        'idList': trello.list_ids.not_started,
-    })
-    r.raise_for_status()
-    updated_card = r.json()
-    return Item.from_trello(trello, updated_card)
+    def complete_item(self, id):
+        r = self.trello.request(f'/cards/{id}', 'PUT', {
+            'idList': self.trello.list_ids.done,
+        })
+        r.raise_for_status()
+        updated_card = r.json()
+        return Item.from_trello(self.trello, updated_card)
+
+
+    def reset_item(self, id):
+        r = self.trello.request(f'/cards/{id}', 'PUT', {
+            'idList': self.trello.list_ids.not_started,
+        })
+        r.raise_for_status()
+        updated_card = r.json()
+        return Item.from_trello(self.trello, updated_card)
